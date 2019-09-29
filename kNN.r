d07826cd6afa9ec15e13a1865cb6850b784fc9cf
vovaa99 <vovaa99@users.noreply.github.com>
2019-09-29 21:28:45 +0300

    kNN method implementation

---
+++ kNN.r
@@ -0,0 +1,47 @@
+## Евклидово расстояние
+euclideanDistance <- function(u, v)
+{
+  sqrt(sum((u - v)^2))
+}
+
+## Сортируем объекты согласно расстояния до объекта z
+sortObjectsByDist <- function(xl, z, metricFunction = euclideanDistance)
+{
+  l <- dim(xl)[1]
+  n <- dim(xl)[2] - 1
+  ## Создаём матрицу расстояний
+  distances <- matrix(NA, l, 2)
+  for (i in 1:l)
+  {
+    distances[i, ] <- c(i, metricFunction(xl[i, 1:n], z))
+  }
+  ## Сортируем
+  orderedXl <- xl[order(distances[, 2]), ]
+  return (orderedXl);
+}
+
+## Применяем метод kNN
+kNN <- function(xl, z, k)
+{
+  ## Сортируем выборку согласно классифицируемого	объекта
+  orderedXl <- sortObjectsByDist(xl, z)
+  n <- dim(orderedXl)[2] - 1
+  ## Получаем классы первых k соседей
+  classes <- orderedXl[1:k, n + 1]
+  ## Составляем таблицу(+считает общее число элементов каждого "типа") встречаемости каждого класса
+  counts <- table(classes)
+  ## Находим класс, который чаще всего встречается среди первых k соседей
+  class <- names(which.max(counts))
+  return (class)
+}
+
+colors <- c("setosa" = "red", "versicolor" = "green3", "virginica" = "blue")
+plot(iris[, 3:4], pch = 21, bg = colors[iris$Species], col = colors[iris$Species], asp = 1)
+
+##set the 
+z <- c(2.7,1)
+xl <- iris[,3:5]
+
+##kNN
+class <- kNN(xl, z, k=6)
+points(z[1], z[2], pch = 22, bg = colors[class], asp = 1)
