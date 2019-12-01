240c371976ccc60c03d6ea300ec47bb326504725
vovaa99 <vovaa99@users.noreply.github.com>
2019-12-02 00:42:21 +0300

    upd

--- baes/density_function_curves/curves.r
+++ baes/density_function_curves/curves.r
@@ -2,34 +2,37 @@ line <- function(m,A)
 {
 	determ<-det(A)
 	
-	a <- A[2,2]/determ
-	b <- -A[2,1]/determ
-	c <- -A[1,2]/determ
-	d <- A[1,1]/determ
+	sol<-solve(A)
 	
-	x0 <- m[1]
-	y0 <- m[2]
-  
+	c <- sol[1,1]
+	d <- sol[1,2]
+	e <- sol[2,1]
+	f <- sol[2,2]
+	
+	g <- m[1]
+	h <- m[2]
+	
 	x <- seq(-2.5, 2.5, 0.1)
 	y <- seq(-2.5, 2.5, 0.1)
 	
-	A <- d
-	B <- a
-	C <- -c-b
-	D <- -2*d*x0+y0*(c+b)
-	E <- -2*a*y0+x0*(c+b)
-	F <- d*x0^2+a*y0^2+x0*y0*(-c-b)
+	A <- c
+	B <- f
+	C <- e+d
+	D <- -2*g*c-h*e-h*d
+	E <- -2*h*f-g*e-g*d
+	F <- g*g*c-g*h*e+g*h*d+h*h*f
 	
 	func <- function(x, y) {
-    	1/(2*pi*sqrt(determ))*exp((-1/2)*(x^2*A + y^2*B + x*y*C + x*D + y*E + F))
+	  1/(2*pi*sqrt(determ))*exp((-1/2)*(x^2*A + y^2*B + x*y*C + x*D + y*E + F))
 	}
 	
 	z <- outer(x, y, func)
-  
+	
 	contour(x, y, z)
 }
 
 line(c(0,0),matrix(c(1,0,0,0.4),2,2))
+
 line(c(0,0),matrix(c(0.4,0,0,1),2,2))
-line(c(0,0),matrix(c(1,1,0,1),2,2))
+line(c(0,0),matrix(c(1,0,1,1),2,2))
   
